38929e6143e97fcef8062fd0f892e367625662a9
Your Name <your.email@example.com>
2026-08-02 23:06:55 +0530

    stream preview script done, progress on notebook

---
+++ stream_preview.py
@@ -0,0 +1,32 @@
+import cv2
+import os
+import uuid
+
+os.chdir("D:/Facial Recognition/")
+
+POSITIVES = os.path.join("data", "positive")
+NEGATIVES = os.path.join("data", "negative")
+ANCHORS = os.path.join("data", "anchors")
+dirs = [POSITIVES, NEGATIVES, ANCHORS]
+for dir in dirs :
+    os.makedirs(dir, exist_ok = True)
+
+cap = cv2.VideoCapture(0)
+while True:
+    ret, frame = cap.read()
+    cv2.imshow("Frame", frame)
+    if cv2.waitKey(1) & 0xFF == ord('a'):
+        imgname = os.path.join(ANCHORS, "{}.jpg".format(uuid.uuid1()))
+        cv2.imwrite(imgname, frame)
+        # collecting the positives
+    if (cv2.waitKey(1) & 0xFF == ord('p')):
+        imgname = os.path.join(POSITIVES, "{}.jpg".format(uuid.uuid1()))
+        cv2.imwrite(imgname, frame)
+        # collecting the negatives
+    if (cv2.waitKey(1) & 0xFF == ord('n')):
+        imgname = os.path.join(NEGATIVES, "{}.jpg".format(uuid.uuid1()))
+        cv2.imwrite(imgname, frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
